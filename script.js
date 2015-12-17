9ae31901849558b899fdf70d88f3c09529cb9f22
Rico Hageman <info@ricohageman.nl>
2015-12-17 19:01:44 +0100

    Added the functionality to add the header

--- script.js
+++ script.js
@@ -2,6 +2,20 @@ var device_ID = '__YOUR_PARTICLE_DEVICE_ID__ ';
 var access_token = '__YOUR_PARTICLE_ACCESS_TOKEN__';
 var variables = [''];
 
+//  Select the active spreadsheet
+var sheet = SpreadsheetApp.getActiveSheet();
+
+//	Function to create a header
+function showHeader()	{
+	//	At first add the timestamp
+	sheet.getRange(1,1).setValue('Timestamp');
+
+	//	Loop through all the variables and add the names of them
+	for (var i = 0; i < variables.length; i++) {
+		sheet.getRange(1,1 + 1 + i).setValue(variables[i]);
+	};
+}
+
 //  Function to get and parse the JSON variable from the spark API
 function getResponse(variable){
 	var JSONResponse = UrlFetchApp.fetch("https://api.spark.io/v1/devices/" + device_ID + "/" + variable + "?access_token=" + access_token);
@@ -15,9 +29,6 @@ function collectData()  {
 	var timestamp = new Date();
 	var results = [timestamp];
 	var resultsWithKey = [];
-
-	//  Select the active spreadsheet
-	var sheet = SpreadsheetApp.getActiveSheet();
 
 	//	Loop through the array with variables and get their content
 	for (var i = 0; i < variables.length; i++) {
