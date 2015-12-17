07228907d1237c9d0a3a168d6aa6c934b3394832
Rico Hageman <info@ricohageman.nl>
2015-12-17 19:17:46 +0100

    Added the showing the latest data on top of the page and added a way to loop the script

--- script.js
+++ script.js
@@ -1,18 +1,44 @@
 var device_ID = '__YOUR_PARTICLE_DEVICE_ID__ ';
 var access_token = '__YOUR_PARTICLE_ACCESS_TOKEN__';
 var variables = [''];
+var checkLatestData = true;
 
 //  Select the active spreadsheet
 var sheet = SpreadsheetApp.getActiveSheet();
 
 //	Function to create a header
 function showHeader()	{
+	//	Clear the first row
+	sheet.deleteRow(1);
+
 	//	At first add the timestamp
 	sheet.getRange(1,1).setValue('Timestamp');
 
 	//	Loop through all the variables and add the names of them
 	for (var i = 0; i < variables.length; i++) {
-		sheet.getRange(1,1 + 1 + i).setValue(variables[i]);
+		sheet.getRange(1,2 + i).setValue(variables[i]);
+	};
+
+	if(checkLatestData) {
+		sheet.getRange(1, 3 + variables.length).setValue('Timestamp');
+
+		//	Loop through all the variables and add the names of them
+		for (var i = 0; i < variables.length; i++) {
+			sheet.getRange(1,4 + variables.length).setValue(variables[i]);
+		};
+	}
+
+	collectData();
+}
+
+//	Function to show the latest data
+function showLatestData(timestamp, results)	{
+	//	At first add the timestamp
+	sheet.getRange(2,3 + variables.length).setValue(timestamp);
+
+	//	Loop through all the variables and add the names of them
+	for (var i = 0; i < results.length; i++) {
+		sheet.getRange(2,4 + variables.length).setValue(results[i]);
 	};
 }
 
@@ -37,7 +63,12 @@ function collectData()  {
 		results.push(getResponse(variables[i]));
 		resultsWithKey[variables[i]] = getResponse(variables[i]);
 	};
+
+	if(checkLatestData) showLatestData(timestamp, results)
 	
 	//  Add the responses to the spreadsheet
 	sheet.appendRow(results);
+
+	Utilities.sleep(5 * 1000)
+	collectData();
 }
